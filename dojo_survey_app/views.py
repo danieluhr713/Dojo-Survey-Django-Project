c12214f6e71ec40c04b821ff68182d6d6f9fbd4e
danieluhr713 <85532956+danieluhr713@users.noreply.github.com>
2021-06-24 22:57:09 -0500

    Update views.py

--- dojo_survey_app/views.py
+++ dojo_survey_app/views.py
@@ -5,12 +5,20 @@ def index(request):
     return render(request, "index.html")
 
 def create(request):
+    request.session['name'] = request.POST['full_name']
+    request.session['location'] = request.POST['Dojo_location']
+    request.session['languages'] = request.POST['Dojo_language']
+    request.session['comments'] = request.POST['optionalComment']
+    request.session['engineer'] = request.POST['SoftwareEngineer']
+    return redirect('/dojo_dash')
+
+def dashboard(request):
     context = {
-        'name': request.POST['full_name'],
-        'location': request.POST['Dojo_location'],
-        'languages': request.POST['Dojo_language'],
-        'comments': request.POST['optionalComment'],
-        'engineer': request.POST['SoftwareEngineer']
+        'name': request.session['name'],
+        'location': request.session['location'],
+        'languages': request.session['languages'],
+        'comments': request.session['comments'],
+        'engineer': request.session['engineer']
     }
     return render(request, 'dojo.html', context)
 
